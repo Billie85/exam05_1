6c167f6811078970ff7dc7214d94b58537371ee9
kayumi <kayumi70@gmail.com>
2023-11-17 22:44:34 +0900

    new

---
+++ 0/Warlock.hpp
@@ -0,0 +1,28 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+#include <string>
+#include <map>
+#include "ASpell.hpp"
+#include "ATarget.hpp"
+
+class Warlock {
+	private:
+		std::string _name;
+		std::string _title;
+		//not use
+		Warlock();
+		Warlock(Warlock const &piyo);
+		Warlock &operator=(Warlock const &piyo);
+		std::map < std::string, ASpell *> _book;
+	public:
+		Warlock(std::string const &name, std::string const &title);
+		const std::string &getName() const;
+		const std::string &getTitle() const;
+		void setTitle(const std::string &newTitle);
+		void introduce() const;
+		~Warlock();
+};
+
+#endif
